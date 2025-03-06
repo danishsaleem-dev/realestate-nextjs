620bea8bf22173a7a6c9730a8581261bf0aba63b
danishsaleem-dev <danishsaleem.dev@gmail.com>
2025-03-06 19:27:34 +0500

    6th March 25 - Updates

--- src/components/Item/ItemBody/Property360.tsx
+++ src/components/Item/ItemBody/Property360.tsx
@@ -1,22 +1,7 @@
-"use client";
-import { useEffect } from "react";
 
-import CloudImage360 from "js-cloudimage-360-view";
 
 const Property360 = () => {
-    useEffect(() => {
-        const instance = new CloudImage360();
-        
-        const container = document.getElementById("property-360");
-        if (container) {
-          instance.init(container, {
-            folder: "https://scaleflex.cloudimg.io/v7/demo/suv-orange-car-360/", // Change this to your image folder
-            filenameX: "orange-{index}.jpg", // Image naming format (adjust accordingly)
-            amountX: 73, // Total number of images
-            responsive: "scaleflex",
-          });
-        }
-      }, []);
+    
     
       return <div id="property-360" className="w-full h-auto md:h-[500px] rounded-2xl"></div>;
 }
