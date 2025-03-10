62932bbb0d8a1799242dcdc798dbeb06e419015f
danishsaleem-dev <danishsaleem.dev@gmail.com>
2025-03-10 17:40:17 +0500

    updates

--- src/components/Home/FeaturedIn/FeaturedIn.tsx
+++ src/components/Home/FeaturedIn/FeaturedIn.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import SectionHeading from '@/components/Helper/SectionHeading'
+import Image from 'next/image'
 
 const FeaturedIn = () => {
   return (
@@ -8,12 +9,13 @@ const FeaturedIn = () => {
             <SectionHeading heading='Featured In' subheading='' description='' />
             <div className='mt-10 md:mt-5'>
                 <div className='flex flex-wrap md:flex-nowrap justify-center items-center gap-10 md:gap-4'>
-                    <img src='/images/press_1.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
-                    <img src='/images/press_2.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
-                    <img src='/images/press_3.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
-                    <img src='/images/press_4.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
-                    <img src='/images/press_5.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
-                    <img src='/images/press_6.webp' className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4' />
+                  <Image src='/images/press_1.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                  <Image src='/images/press_2.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                  <Image src='/images/press_3.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                  <Image src='/images/press_4.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                  <Image src='/images/press_5.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                  <Image src='/images/press_6.webp' alt='Featured' width={200} height={200} className='w-[40%] md:w-[20%] h-10 md:h-14 object-contain md:p-4'/>
+                    
                 </div>
             </div>
         </div>   
--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
 import BannerSearch from './BannerSearch'
 
 const Hero = () => {
